e7de3005cfccfd88de6afcf415e205294f63fa4b
Rohit <potanohit12@gmail.com>
2024-12-04 10:16:37 +0530

    added sigup backend api

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -3,8 +3,10 @@ const express= require("express")
 const router=express.Router()
 
 const {sendotp}= require("../controllers/Auth");
+const {signup}=require("../controllers/Auth")
 
 
 router.post("/sendotp",sendotp)
+router.post("/signup",signup)
 
 module.exports=router;
